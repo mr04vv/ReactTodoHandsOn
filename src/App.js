398ad967084640830df23efdb4223731d4731852
mr04vv <mr04vv@gmail.com>
2019-08-31 21:39:16 +0900

    finished todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './pages/Todo';
+import styled from 'styled-components'
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [finishedList, setFinishedList] = useState([]);
+
+  const addTodo = () => {
+    if (!!input) {
+      setTodoList([...todoList, input]);
+      setInput('');
+    }
+  }
+
+  const deleteTodo = (index) => {
+    setTodoList(todoList.filter((_, idx) => idx !== index))
+  }
+
+  const deleteFinishTodo = (index) => {
+    setFinishedList(finishedList.filter((_, idx) => idx !== index))
+  }
+
+  const finishTodo = (index) => {
+    deleteTodo(index)
+    setFinishedList([...finishedList,todoList.find((_, idx) => idx === index)])
+  }
+
+  const reopenTodo = (index) => {
+    deleteFinishTodo(index)
+    setTodoList([...todoList,finishedList.find((_, idx) => idx === index)])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(e) => setInput(e.target.value)} value={input}/>
+      <button onClick={() => addTodo()}>追加</button>
+      <TodoContainer>
+        <Todo todoList={todoList} deleteTodo={deleteTodo} changeTodoStatus={finishTodo}/>
+        <Todo todoList={finishedList} deleteTodo={deleteFinishTodo} changeTodoStatus={reopenTodo}/>
+      </TodoContainer>
     </div>
   );
 }
 
 export default App;
+
+const TodoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  margin: 0 auto;
+  justify-content: space-between;
+`;
